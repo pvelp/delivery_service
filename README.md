25650a5bd5b67a3e5ca13985b5ba1ca58cce354c
korchaginks <kkonstantinserg@gmail.com>
2024-02-01 19:08:43 +0300

    -Изменение настроек на локальный доступ к БД -Добавление команды на создание суперюзера -Докер и Докеркомпоуз файлы -Инструкция по запуску контейнера

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # delivery_service
+
+## Поднятие докерфайла:
+- docker-compose build
+- docker-compose up
+- Далее открываете вторую консоль, так как предыдущая занята работой сервера
+- docker ps
+- Находите id для delivery_service:dev_back и копируете его
+- docker exec -it <id> python backend/manage.py csu
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,11 +131,11 @@ DJOSER = {
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': 'postgres',
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
