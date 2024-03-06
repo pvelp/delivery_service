186a7e0bd79268d76c49b7d78e8cb0d2ef973ffe
korchaginks <kkonstantinserg@gmail.com>
2024-03-06 23:32:52 +0300

    Добавил тесты для добавления товара и внес изменения в контроллер: добавил проверку на добавление спрятанного товара, изменил ответ сервера на добавление товара Добавил тест удаления товара из корзины и внес изменения в его контроллер: -удаляется 1 штука товара при отправке запроса -изменил ответа сервера на удаление

--- backend/main/tests.py
+++ backend/main/tests.py
@@ -180,3 +180,248 @@ class ProductRetrieveTestCase(APITestCase):
             response.json(),
             {'detail': 'Product is not available'}
         )
+
+
+class AddToCartTestCase(APITestCase):
+
+    def setUp(self):
+        self.add_to_cart_url = 'http://localhost:8000/add-to-cart/'
+        self.category1 = Category.objects.create(
+            title='Шашлыки'
+        )
+        self.category2 = Category.objects.create(
+            title='Напитки'
+        )
+        self.product1 = Product.objects.create(
+            title='шашлык1',
+            price=500,
+            category=self.category1
+        )
+        self.product2 = Product.objects.create(
+            title='шашлык3',
+            price=400,
+            category=self.category1,
+            is_hidden=True
+        )
+        self.product_1_response = {
+            'product_id': self.product1.id
+        }
+        self.product_2_response = {
+            'product_id': self.product2.id
+        }
+        self.product_404_response = {
+            'product_id': 10
+        }
+
+    def test_add_to_cart_201(self):
+
+        response = self.client.post(
+            self.add_to_cart_url,
+            self.product_1_response,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    "message": "Product added to cart successfully"
+                },
+                {
+                    "cart_id": 1,
+                    "product_id": self.product1.id,
+                    "quantity": 1
+                }
+            ]
+        )
+
+    def test_add_to_cart_400(self):
+
+        response = self.client.post(
+            self.add_to_cart_url,
+            self.product_2_response,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "error": "Cannot added hidden product"
+            }
+        )
+
+    def test_add_to_cart_404(self):
+
+        response = self.client.post(
+            self.add_to_cart_url,
+            self.product_404_response,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "error": "Product not found"
+            }
+        )
+
+
+class RemoveFromCartTestCase(APITestCase):
+
+    def setUp(self):
+        self.add_to_cart_url = 'http://localhost:8000/add-to-cart/'
+        self.remove_from_cart_url = 'http://localhost:8000/remove-from-cart/'
+        self.category1 = Category.objects.create(
+            title='Шашлыки'
+        )
+        self.category2 = Category.objects.create(
+            title='Напитки'
+        )
+        self.product1 = Product.objects.create(
+            title='шашлык1',
+            price=500,
+            category=self.category1
+        )
+        self.product2 = Product.objects.create(
+            title='шашлык3',
+            price=400,
+            category=self.category1,
+            is_hidden=True
+        )
+        self.product_1_response = {
+            'product_id': self.product1.id
+        }
+        self.product_2_response = {
+            'product_id': self.product2.id
+        }
+        self.product_404_response = {
+            'product_id': 10
+        }
+
+    def test_remove_from_cart_200(self):
+
+        self.client.post(
+            self.add_to_cart_url,
+            self.product_1_response,
+        )
+
+        response = self.client.post(
+            self.remove_from_cart_url,
+            self.product_1_response
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    "message": "Product removed from cart successfully"
+                },
+                {
+                    "details": {
+                        "cart_id": 4,
+                        "product_id": self.product1.id,
+                        "quantity": 0
+                    }
+                }
+            ]
+        )
+
+    def test_remove_from_cart_404(self):
+        """
+        Test remove from cart, but product not in cart. Product's quantity = 0
+        """
+
+        self.client.post(
+            self.add_to_cart_url,
+            self.product_1_response,
+        )
+
+        self.client.post(
+            self.remove_from_cart_url,
+            self.product_1_response
+        )
+
+        response = self.client.post(
+            self.remove_from_cart_url,
+            self.product_1_response
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "error": "Product is not in the cart"
+            }
+        )
+
+    def test_remove_from_cart_404_not_found(self):
+        """
+        Test remove from cart, but product doesn't exist
+        """
+
+        #  Добавляю любой товар, чтобы создать коризну для пользователя
+
+        self.client.post(
+            self.add_to_cart_url,
+            self.product_1_response
+        )
+
+        response = self.client.post(
+            self.remove_from_cart_url,
+            self.product_404_response,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "error": "Product not found"
+            }
+        )
+
+    def test_remove_from_cart_not_found(self):
+        """
+        Test remove from cart, but cart doesn't exist
+        """
+
+        response = self.client.post(
+            self.remove_from_cart_url,
+            self.product_404_response,
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "error": "Cart not found"
+            }
+        )
--- backend/main/views.py
+++ backend/main/views.py
@@ -47,7 +47,8 @@ class ProductRetrieveAPIView(RetrieveAPIView):
 class AddToCart(APIView):
     def post(self, request):
         product_id = request.data.get('product_id')
-        quantity = request.data.get('quantity', 1)  # Если количество не указано, то по умолчанию 1, предполагается нажатие на "+"
+        quantity = request.data.get('quantity',
+                                    1)  # Если количество не указано, то по умолчанию 1, предполагается нажатие на "+"
 
         if request.user.is_authenticated:
             user = request.user
@@ -62,11 +63,21 @@ class AddToCart(APIView):
 
         try:
             product = Product.objects.get(pk=product_id)
+            if product.is_hidden:
+                return Response({'error': 'Cannot added hidden product'}, status=status.HTTP_400_BAD_REQUEST)
+
             cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
             cart_item.quantity += quantity
             cart_item.save()
-            serializer = CartItemSerializer(cart_item)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            response_data = [
+                {"message": "Product added to cart successfully"},
+                {
+                    'cart_id': cart.id,
+                    'product_id': cart_item.product.id,
+                    'quantity': cart_item.quantity
+                }
+            ]
+            return Response(response_data, status=status.HTTP_201_CREATED)
         except Product.DoesNotExist:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
@@ -89,16 +100,26 @@ class RemoveFromCart(APIView):
         except Cart.DoesNotExist:
             return Response({"error": "Cart not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Удаление товара из корзины
         try:
             product = Product.objects.get(pk=product_id)
             cart_item = CartItem.objects.get(cart=cart, product=product)
-            cart_item.delete()
-            return Response({"message": "Product removed from cart successfully"}, status=status.HTTP_200_OK)
+            if cart_item.quantity >= 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+                return Response([
+                    {"message": "Product removed from cart successfully"},
+                    {'details':
+                        {
+                            'cart_id': cart.id,
+                            'product_id': cart_item.product.id,
+                            'quantity': cart_item.quantity
+                        }}
+                    ],
+                    status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Product is not in the cart"}, status=status.HTTP_404_NOT_FOUND)
         except Product.DoesNotExist:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
-        except CartItem.DoesNotExist:
-            return Response({"error": "Product is not in the cart"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CartView(APIView):
@@ -167,7 +188,8 @@ class ApplyPromoCode(APIView):
         user = request.user
         cart, created = Cart.objects.get_or_create(user=user)
         if PromoUsage.objects.filter(user=user, promo=promo).count() >= promo.max_usage_count:
-            return Response({"error": "Maximum usage limit reached for this promo code"}, status=status.HTTP_403_FORBIDDEN)
+            return Response({"error": "Maximum usage limit reached for this promo code"},
+                            status=status.HTTP_403_FORBIDDEN)
 
         cart.promo = promo
         cart.save()
